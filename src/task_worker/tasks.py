136fbc6a2a1c4f0adf1ca110ef99ddf94bc47046
Graham Harrison <gupyfish@gmail.com>
2023-03-30 22:43:08 -0600

    fix resize function

--- src/task_worker/tasks.py
+++ src/task_worker/tasks.py
@@ -34,7 +34,7 @@ def resize_image(self, icon):
     result_keys = []
     for new_width, new_height in resize_sizes:
         # Resizes the image and retains aspect ratio
-        resized_img = PIL.ImageOps.contain(image, (new_width, new_height), method=PIL.Image.LANCZOS)
+        resized_img = resize_image(image, new_width, new_height)
         img_byte_arr = io.BytesIO()
         resized_img.save(img_byte_arr, format='PNG')
         img_byte_arr = img_byte_arr.getvalue()
@@ -44,6 +44,42 @@ def resize_image(self, icon):
 
     pipeline.execute()
     return {'data':result_keys}
+
+def resize_image(image, new_width, new_height):
+    # Get original image size
+    width, height = image.size
+
+    # Calculate aspect ratio
+    aspect_ratio = width / height
+
+    # Calculate new aspect ratio
+    new_aspect_ratio = new_width / new_height
+
+    # Calculate new size based on aspect ratio
+    if new_aspect_ratio < aspect_ratio:
+        # If new aspect ratio is smaller than original, resize based on width
+        resized_width = new_width
+        resized_height = int(new_width / aspect_ratio)
+    else:
+        # Otherwise, resize based on height
+        resized_width = int(new_height * aspect_ratio)
+        resized_height = new_height
+
+    # Resize image using Lanczos filter
+    resized_img = image.resize((resized_width, resized_height), resample=Image.LANCZOS)
+
+    # Create new image with desired size and background color
+    new_img = Image.new(mode='RGB', size=(new_width, new_height), color=resized_img.getpixel((0, 0)))
+
+    # Calculate offsets for centering resized image
+    x_offset = (new_width - resized_width) // 2
+    y_offset = (new_height - resized_height) // 2
+
+    # Paste resized image onto new image with offsets
+    new_img.paste(resized_img, (x_offset, y_offset))
+
+    # Return resized image
+    return new_img
 
 @app.task
 def hello():
